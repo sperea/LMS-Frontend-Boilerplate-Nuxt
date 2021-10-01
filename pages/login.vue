62e47f7d2de128481278f1f7451b864557e0ffa4
Sergio Perea <sperea@protonmail.com>
2021-10-01 12:15:36 +0200

    Login page created

--- pages/login.vue
+++ pages/login.vue
@@ -4,7 +4,6 @@
 <!-- Create By Joker Banny -->
 <div class="min-h-screen bg-no-repeat bg-cover bg-center"
 	style="background-image: url('https://images.unsplash.com/photo-1486520299386-6d106b22014b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80')">
-    
 	<div class="flex justify-end">
 		<div class="bg-white min-h-screen w-1/2 flex justify-center items-center">
 			<div>
